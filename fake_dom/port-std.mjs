3bf279a4b3fdc989ad220024f002cb8604cf7dd5
okuoku <mjt@cltn.org>
2025-12-21 19:54:17 +0900

    (Tentative) Added missing source

---
+++ fake_dom/port-std.mjs
@@ -0,0 +1,10 @@
+/* Copied from https://github.com/okuoku/cwgl-proto/blob/e34dffe06ac23a9797dd9ecd601433c44ab79cdd/jstestapp/port-std.js */
+import fs from "fs";
+import crypto from "crypto";
+import perf_hooks from "perf_hooks";
+
+export default {
+    performance_now: perf_hooks.performance.now,
+    fs_readFileSync: fs.readFileSync,
+    crypto_randomFillSync: crypto.randomFillSync,
+};
